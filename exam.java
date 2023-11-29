32895c1fe564c74f78c065007e8e2eac7842df07
Rishi100304 <rishianand1018gmail.com>
2023-11-29 13:00:12 +0530

    First Commit

---
+++ exam.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class exam {
+public static void main(String[] args) {
+    int a,b,c;
+    a=10;
+    b=5;
+    c=a*b;
+    System.out.println("Prouct of two numbers = "+c);
+}
+}
